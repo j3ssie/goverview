436ecd1b327217951d7de9b471b3e0bd9e86c4fa
j3ssie <j3ssiejjj@gmail.com>
2021-07-10 19:40:27 +0700

    Add new report mode

--- cmd/report.go
+++ cmd/report.go
@@ -12,14 +12,12 @@ func init() {
 		Short: "Generate HTML Report based on screenshot output",
 		RunE:  runReport,
 	}
-	reportCmd.Flags().StringVar(&options.ReportFile, "report", "", "Report name")
+	reportCmd.Flags().StringVar(&options.ReportFile, "report", "report.html", "Report name")
 	RootCmd.AddCommand(reportCmd)
 }
 
 func runReport(_ *cobra.Command, _ []string) error {
-	if options.ReportFile == "" {
-		options.ReportFile = path.Join(options.Output, "report.html")
-	}
+	options.ReportFile = path.Join(options.Output, "report.html")
 	core.RenderReport(options)
 	return nil
 }
